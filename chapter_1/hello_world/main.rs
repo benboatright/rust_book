ff982d388ccd6d8c0fa88ff58994d4517a93de10
Ben Boatright <benboatright@Bens-MacBook-Pro.local>
2024-07-30 17:12:15 -0400

    add hello world

---
+++ chapter_1/hello_world/main.rs
@@ -0,0 +1,9 @@
+// Hello, World
+// https://rust-book.cs.brown.edu/ch01-02-hello-world.html
+
+// Main is always the first code that runs 
+// in an executable Rust program.
+fn main() {
+    //
+    println!("Hello, world!");
+}
